1c9cecee82291beb3a3309c2e8a66b2c10dc093a
mariohmis <138615425+MarioHMis@users.noreply.github.com>
2025-04-01 11:44:55 -0600

    ading especifi exceptions

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 
 def home(request):
@@ -20,14 +21,19 @@ def signup(request):
                 user = User.objects.create_user(
                     username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse('User created successfully')
-            except:
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
                 return render(request, 'signup.html', {
                     'form': UserCreationForm,
-                    "error": "Username already exist"
+                    "error": 'User already exists'
                 })
 
         return render(request, 'signup.html', {
             'form': UserCreationForm,
             "error": 'Password do not match'
         })
+
+
+def tasks(request):
+    return render(request, 'tasks.html')
