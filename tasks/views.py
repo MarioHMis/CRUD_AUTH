ca1abf102d2a6a7bcf121d8ec2d2976e9ab96e4d
mariohmis <138615425+MarioHMis@users.noreply.github.com>
2025-04-01 11:26:33 -0600

    manage errors

--- tasks/views.py
+++ tasks/views.py
@@ -3,27 +3,31 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 
+
 def home(request):
     return render(request, 'home.html')
-    
 
 
 def signup(request):
 
     if request.method == 'GET':
         return render(request, 'signup.html', {
-        'form': UserCreationForm
-    })
-
+            'form': UserCreationForm
+        })
     else:
         if request.POST['password1'] == request.POST['password2']:
-            #register user
             try:
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
-            user.save()
-            return HttpResponse('User created successfully')
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse('User created successfully')
             except:
-                return HttpResponse('Username already exist')
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    "error": "Username already exist"
+                })
 
-            
-        return HttpResponse('Passwprd do not match')
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            "error": 'Password do not match'
+        })
